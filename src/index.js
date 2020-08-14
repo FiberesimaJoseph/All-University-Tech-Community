e0781304ae2b60f55b9e4c75774bb63e89fe8595
Tekena Solomon <tekena157@gmail.com>
2020-08-14 19:50:36 +0100

    finished appBar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/Navbar.css'
+import './styles/AppBar.css'
 import './App.css'
 import App from './App'
 
