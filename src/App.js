a7cedc629cded12f6b8495163147ba432a66f0fb
FiberesimaJoseph <sctbilly@gmail.com>
2020-08-14 13:59:40 -0600

    About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Component/Navbar";
+import AboutUs from "./Component/AboutUs";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <AboutUs />
     </div>
   );
 };
