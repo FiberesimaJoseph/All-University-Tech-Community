5fab765b420e98c33fe61a06b05cea1583ae3e46
Tekena Solomon <tekena157@gmail.com>
2020-08-14 20:50:00 +0100

    added colourful content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const App = () => {
   return (
     <div className="App">
       <AppBar />
+      <div className="container">
+        <div className="colored-content-container">
+          <div className="content content-blue">Keynote</div>
+          <div className="content content-green">Product Pitches</div>
+          <div className="content content-orange">Hackathon</div>
+          <div className="content content-red">Speaker Sessions</div>
+          <div className="content content-red">Product Launch</div>
+          <div className="content content-blue codelab">Codelab Sessions</div>
+          <div className="content content-green technology">
+            Technology Entepreneurship
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
