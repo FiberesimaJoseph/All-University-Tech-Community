71396fd592e2172b46f2b5a30862379ea3d50666
Tekena Solomon <tekena157@gmail.com>
2020-08-15 21:00:57 +0100

    adding audience analysis

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AppBar from './components/AppBar'
 import AboutUs from './components/AboutUs'
 import ColoredContainer from './components/ColoredContainer'
+import AudienceAnalysis from './components/AudienceAnalysis'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <div className="container">
         <ColoredContainer />
         <AboutUs />
+        <AudienceAnalysis />
       </div>
     </div>
   )
---
+++ src/components/AudienceAnalysis.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AudienceAnalysis = () => {
+  return (
+    <div className="main-content">
+      <p className="location">Port-Hacourt, Nigeria</p>
+      <div className="audience-analysis">
+        <p>Audience Analysis</p>
+        <p>Uniting Students across Nigeria with Technology</p>
+        <div className="compass">
+          <p>
+            <span>North-Central </span>
+            <span>North-East </span>
+            <span>North-West </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AudienceAnalysis
