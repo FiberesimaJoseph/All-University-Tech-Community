094d9f466e033d164d0a3cfb02db8a3ca9a62887
Tekena Solomon <tekena157@gmail.com>
2020-08-15 20:45:06 +0100

    finished about and color-content style

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,12 +3,14 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <h1>All University Tech Conference</h1>
+      <h1>
+        All University Tech <span className="conference">Conference</span>
+      </h1>
       <p>
-        is a conference gathering techology enthusiast,and pratitioners within
-        Nigeria institutions from ND to PHD students for the share of ideas, and
-        exposure to opportunities. it's amies to unite students across the
-        geo-political zones of Nigeria through techology.
+        (AUTC), is a conference gathering techology enthusiast and pratitioners
+        within Nigeria institutions from ND to PHD students for the share of
+        ideas, and exposure to opportunities. it's amies to unite students
+        across the geo-political zones of Nigeria through techology.
       </p>
     </div>
   )
