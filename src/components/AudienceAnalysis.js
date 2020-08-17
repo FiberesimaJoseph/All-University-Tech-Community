93ede56d24f2db2b9b78577bc6e141c699d06459
Tekena Solomon <tekena157@gmail.com>
2020-08-17 11:48:42 +0100

    finished compass and roles

--- src/components/AudienceAnalysis.js
+++ src/components/AudienceAnalysis.js
@@ -18,10 +18,34 @@ const AudienceAnalysis = () => {
         </p>
         <div className="compass">
           <p className="c1">
-            <span className="nc">North-Central </span>
-            <span className="ne">North-East </span>
-            <span className="nw">North-West </span>
+            <span className="nc">North-Central</span>
+            <span className="ne">North-East</span>
+            <span className="nw">North-West</span>
           </p>
+          <p className="c2">
+            <span className="ss">South-South</span>
+            <span className="se">South-East</span>
+            <span className="sw">South-West</span>
+          </p>
+        </div>
+        <div className="role">
+          <div className="icons-container">
+            <img
+              src="https://img.icons8.com/dotty/80/000000/toilet.png"
+              alt="MALE/FEMALE"
+            />
+          </div>
+          <div className="roles">
+            <p className="role-header">ROLES</p>
+            <hr />
+            <p className="role-list">
+              <span className="students">Student</span>
+              <span className="developer">Developer</span>
+              <span className="designer">Designer</span>
+              <span className="enterpreneur">Enterpreneur</span>
+              <span className="others">Others</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
